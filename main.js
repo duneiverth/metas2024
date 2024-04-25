2914217b885910f71af56b0e62478d10949b1fe5
Duda <105284664+duneiverth@users.noreply.github.com>
2024-04-25 10:15:31 -0300

    ccc

--- main.js
+++ main.js
@@ -14,21 +14,6 @@ for (let i = 0; i < botoes.length; i++) {
     }
 }
 
-const botoes = document.querySelectorAll(".botao");
-const textos = document.querySelectorAll(".aba-conteudo");
-
-for (let i = 0; i < botoes.length; i++) {
-    botoes[i].onclick = function () {
-
-        for (let j = 0; j < botoes.length; j++) {
-            botoes[j].classList.remove("ativo");
-            textos[j].classList.remove("ativo");
-        }
-
-        botoes[i].classList.add("ativo");
-        textos[i].classList.add("ativo");
-    }
-}
 //cria as constantes e a lista "tempo" dos objetivos ou metas
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-02-02T00:00:00");
